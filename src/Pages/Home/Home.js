8b886d7dac8ae7e2e49a3f9c4ba077c69e84e228
Nahom Diro <nahomdiroj@gmail.com>
2024-04-27 11:53:11 +0300

    header and footer

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../../componenet/Header/Header'
+import Footer from '../../componenet/Footer/Footer'
+
+const Home = () => {
+  return (
+    <div>
+        <Header/>
+       <Footer/>
+    </div>
+  )
+}
+
+export default Home
